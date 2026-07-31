52bfbfeed46671256c481752494bd166ba14e4a1
406-26-Chayathorn <54558@yupparaj.ac.th>
2026-07-31 10:55:32 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -9,7 +9,6 @@ if st.button("คำนวณ BMI"):
  height_m = height_cm / 100
  bmi = weight / (height_m ** 2)
  
- st.write("---")
  st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
 if bmi < 18.5:
   st.warning("ผอม")
